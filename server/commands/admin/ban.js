d12129d905901ca6ff61d194f42c440cf4aa5199
doyle928 <doyle928@yahoo.com>
2019-11-18 23:36:21 -0500

    changed ban command to 300 and added unbanning and reinviting

--- server/commands/admin/ban.js
+++ server/commands/admin/ban.js
@@ -4,38 +4,35 @@ const randomNumber = require("../../data/randomNumber");
 exports.run = async (client, message, args) => {
   if (!message.member.hasPermission("BAN_MEMBERS")) {
     if (message.guild.id === "559560674246787087") {
-      let randomNum = randomNumber(0, 500);
-      if (randomNum === 500) {
-        if (!args[1]) {
-          message.channel.send("there's no user specified!!!");
+      let randomNum = randomNumber(1, 300);
+      if (randomNum === 300) {
+        let member = null;
+        message.channel.send("lucky number 300 !");
+
+        if (!message.mentions.members.first()) {
+          message.channel.send("you did not mention anyone !!!");
           message.channel.send("<a:02upset:538273249306345476>");
-        } else {
-          let member = null;
-          message.channel.send("lucky number 500 !");
+          return;
+        }
 
-          if (!message.mentions.members.first()) {
-            let id = args[1].replace(/([<>@,#!&])/g, "");
-            try {
-              member = await message.guild.fetchMember(id);
-            } catch {
-              message.channel.send(
-                "I don't think this member exists in the guild"
-              );
-              message.channel.send("<:kanna_confused:607077674099277828>");
-            }
-          } else {
-            member =
-              message.mentions.members.first() ||
-              message.guild.members.get(args[1]);
-          }
+        if (!member.bannable) {
+          message.channel.send("I cannot ban this user!");
+          message.channel.send("<a:sataniacrying:575078717911597077>");
+          return;
+        }
 
-          if (!member.bannable) {
-            message.channel.send("I cannot ban this user!");
-            message.channel.send("<a:sataniacrying:575078717911597077>");
-            return;
-          }
+        message.channel.send(
+          `lucky number 300 !! you are getting banned ${member}!\nyou have 10 secondes until you get banned !`
+        );
+        message.channel.send("<:oh_my:606353903558066176>");
+        setTimeout(() => {
+          member.send(
+            `${message.author.username} has banned you, here is a invite link`
+          );
+          member.send("https://discord.gg/TTVdzRE");
+          let memberId = member.user.id;
           member
-            .ban(reason)
+            .ban("they lost the roulette")
             .then(() => {
               let messageEmbed = new Discord.RichEmbed()
                 .setColor("#fe6860")
@@ -46,6 +43,7 @@ exports.run = async (client, message, args) => {
                 .setTimestamp();
 
               message.channel.send(messageEmbed);
+              message.guild.unban(memberId);
             })
             .catch(error => {
               message.channel.send(
@@ -53,30 +51,38 @@ exports.run = async (client, message, args) => {
               );
               message.channel.send("<:deadinside:606350795881054216>");
             });
-        }
+        }, 10000);
       } else if (randomNum === 1) {
         message.channel.send(
-          `unlucky number 1 !! you are getting banned ! byee !`
+          `unlucky number 1 !! you are getting banned ${message.author.username}! you have 10 secondes until you get banned !`
         );
-        message.author
-          .ban("they lost the roulette")
-          .then(() => {
-            let messageEmbed = new Discord.RichEmbed()
-              .setColor("#fe6860")
-              .setTitle(
-                `${message.author.tag} has been banned by ${message.author.tag}`
-              )
-              .addField("Reason", "they lost the roulette")
-              .setTimestamp();
+        message.channel.send("<:oh_my:606353903558066176>");
+        setTimeout(() => {
+          member.send("you banned youself, here is a invite link");
+          member.send("https://discord.gg/TTVdzRE");
+          let memberId = message.author.id;
 
-            message.channel.send(messageEmbed);
-          })
-          .catch(error => {
-            message.channel.send(
-              `Sorry ${message.author} I couldn't ban the user`
-            );
-            message.channel.send("<:deadinside:606350795881054216>");
-          });
+          message.author
+            .ban("they lost the roulette")
+            .then(() => {
+              let messageEmbed = new Discord.RichEmbed()
+                .setColor("#fe6860")
+                .setTitle(
+                  `${message.author.tag} has been banned by ${message.author.tag}`
+                )
+                .addField("Reason", "they lost the roulette")
+                .setTimestamp();
+
+              message.channel.send(messageEmbed);
+              message.guild.unban(memberId);
+            })
+            .catch(error => {
+              message.channel.send(
+                `Sorry ${message.author} I couldn't ban the user`
+              );
+              message.channel.send("<:deadinside:606350795881054216>");
+            });
+        }, 10000);
       } else {
         message.channel.send(`you got ${randomNum} ! so sad !`);
         // message.channel.send(
