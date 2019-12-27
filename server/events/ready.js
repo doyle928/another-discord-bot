db6b3f449dbcf5c2b7f3130042dc061276b71001
doyle928 <doyle928@yahoo.com>
2019-12-27 13:38:53 -0500

    added reaction verification and welcome message

--- server/events/ready.js
+++ server/events/ready.js
@@ -1,4 +1,4 @@
-module.exports = client => {
+module.exports = async client => {
   console.log("started");
 
   client.user.setStatus("idle");
@@ -7,7 +7,6 @@ module.exports = client => {
     type: 3
   });
 
-    let s = client.guilds.get("559560674246787087");
+  let s = client.guilds.get("559560674246787087");
   await s.channels.get("559709338638352405").fetchMessage("596040210559664139");
-
 };
