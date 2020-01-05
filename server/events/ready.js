a3b3e88bf2e9bcd19a3ed046283e9d219e0078f8
doyle928 <doyle928@yahoo.com>
2020-01-05 03:31:25 +0100

    final of message reaction add/remove

--- server/events/ready.js
+++ server/events/ready.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 module.exports = async client => {
   console.log("started");
 
@@ -16,74 +18,88 @@ module.exports = async client => {
     .fetchMessage("663149701687672862")
     .then(msg => {
       //age
-      msg.react("1️⃣");
-      msg.react("2️⃣");
-      msg.react("3️⃣");
-      msg.react("4️⃣");
-      msg.react("5️⃣");
+      if (_.size(msg.reactions) === 0) {
+        msg.react("1️⃣");
+        msg.react("2️⃣");
+        msg.react("3️⃣");
+        msg.react("4️⃣");
+        msg.react("5️⃣");
+      }
     });
   await s.channels
     .get("561423217709940770")
     .fetchMessage("663150060904644608")
     .then(msg => {
       //personality
-      msg.react("🤐");
-      msg.react("🥳");
-      msg.react("😜");
+      if (_.size(msg.reactions) === 0) {
+        msg.react("🤐");
+        msg.react("🥳");
+        msg.react("😜");
+      }
     });
   await s.channels
     .get("561423217709940770")
     .fetchMessage("663150398458167306")
     .then(msg => {
       //gaming
-      msg.react("🅿");
-      msg.react("❎");
-      msg.react("🍄");
-      msg.react("🖥");
-      msg.react("📱");
+      if (_.size(msg.reactions) === 0) {
+        msg.react("🅿");
+        msg.react("❎");
+        msg.react("🍄");
+        msg.react("🖥");
+        msg.react("📱");
+      }
     });
   await s.channels
     .get("561423217709940770")
     .fetchMessage("663150874184646713")
     .then(msg => {
       //relationship
-      msg.react("💁‍♀️");
-      msg.react("❤");
-      msg.react("🙊");
+      if (_.size(msg.reactions) === 0) {
+        msg.react("💁‍♀️");
+        msg.react("❤");
+        msg.react("🙊");
+      }
     });
   await s.channels
     .get("561423217709940770")
     .fetchMessage("663151089054646315")
     .then(msg => {
       //dm
-      msg.react("✅");
-      msg.react("❌");
-      msg.react("❓");
+      if (_.size(msg.reactions) === 0) {
+        msg.react("✅");
+        msg.react("❌");
+        msg.react("❓");
+      }
     });
   await s.channels
     .get("561423217709940770")
     .fetchMessage("663151396727554059")
     .then(msg => {
       //interests
-      msg.react("🍲");
-      msg.react("🐕");
-      msg.react("🌄");
-      msg.react("⚽");
-      msg.react("🎵");
-      msg.react("🚗");
-      msg.react("📚");
-      msg.react("📺");
-      msg.react("💻");
-      msg.react("🌺");
-      msg.react("🖌️");
-      msg.react("🎮");
-      msg.react("👗");
+      if (_.size(msg.reactions) === 0) {
+        msg.react("🍲");
+        msg.react("🐕");
+        msg.react("🌄");
+        msg.react("⚽");
+        msg.react("🎵");
+        msg.react("🚗");
+        msg.react("📚");
+        msg.react("📺");
+        msg.react("💻");
+        msg.react("🌺");
+        msg.react("🖌️");
+        msg.react("🎮");
+        msg.react("👗");
+      }
     });
   await s.channels
     .get("561423217709940770")
     .fetchMessage("663153065565618190")
     .then(msg => {
       //voicechat
-      msg.react("🎙️");
+      if (_.size(msg.reactions) === 0) {
+        msg.react("🎙️");
+      }
     });
 };
