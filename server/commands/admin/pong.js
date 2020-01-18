7da8907540924e93fd7de93060228472032a573e
doyle928 <doyle928@yahoo.com>
2020-01-18 01:34:02 +0100

    changed pong command

--- server/commands/admin/pong.js
+++ server/commands/admin/pong.js
@@ -2,7 +2,9 @@ exports.run = (client, message) => {
   if (message.author.id === "157673412561469440") {
     message.channel.send("ping ??");
   } else {
-    message.channel.send("it is .ping you weirdo !");
-    message.channel.send("<:monoeil:658912400996827146>");
+    if (message.channel.type === "text") {
+      message.channel.send("it is .ping you weirdo !");
+      message.channel.send("<:monoeil:658912400996827146>");
+    }
   }
 };
