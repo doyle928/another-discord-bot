4dfbaa79b9fa2b91a172144a919a13bbec0955ea
doyle928 <doyle928@yahoo.com>
2020-02-18 19:23:13 +0100

    added giving the message id with said emote to the addemote command

--- server/commands/admin/addemote.js
+++ server/commands/admin/addemote.js
@@ -40,7 +40,7 @@ exports.run = async (client, message, args) => {
               console.error(err);
             });
         }
-      } else {
+      } else if (args[1].indexOf(".") > -1) {
         if (args[2]) {
           message.guild
             .createEmoji(args[1], args[2])
@@ -55,6 +55,50 @@ exports.run = async (client, message, args) => {
             "you need to add the name of the emote after the link !"
           );
         }
+      } else if (
+        args[1].replace(/([^0-9])/g, "").length === 17 ||
+        args[1].replace(/([^0-9])/g, "").length === 18
+      ) {
+        message.channel
+          .fetchMessage(args[1].replace(/([^0-9])/g, ""))
+          .then(m => {
+            if (m.content.indexOf("<:") >= 0 || m.content.indexOf("<a:") >= 0) {
+              let emoteArray = m.content.split(":");
+              let emoteId = emoteArray[emoteArray.length - 1].replace(
+                /([^0-9])/g,
+                ""
+              );
+              let url = `https://cdn.discordapp.com/emojis/${emoteId}`;
+              if (m.content === "<a") {
+                url += `.gif`;
+              } else {
+                url += `.png`;
+              }
+              if (args[2]) {
+                message.guild
+                  .createEmoji(url, args[2])
+                  .then(() => message.channel.send("done !"))
+                  .catch(err => {
+                    message.channel.send("i was not able to");
+                    message.channel.send("<:deadinside:665371578359611412>");
+                    console.error(err);
+                  });
+              } else {
+                message.guild
+                  .createEmoji(url, emoteName)
+                  .then(() => message.channel.send("done !"))
+                  .catch(err => {
+                    message.channel.send("i was not able to");
+                    message.channel.send("<:deadinside:665371578359611412>");
+                    console.error(err);
+                  });
+              }
+            }
+          });
+      } else {
+        return message.channel.send(
+          `what emote are you trying to add ?? you have to add that with the command !!\n\nE.g.\n**.addemote :emote: emoteName**\n**.addemote http://image-link.com emoteName**`
+        );
       }
     } else {
       return message.channel.send(
