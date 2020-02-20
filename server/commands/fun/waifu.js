36ed7da1885c020caa408ea00e3185fee05418fe
doyle928 <doyle928@yahoo.com>
2020-02-20 03:53:48 +0100

    changed waifu command to screenshot to not hit site twice

--- server/commands/fun/waifu.js
+++ server/commands/fun/waifu.js
@@ -3,7 +3,7 @@ const Nightmare = require("nightmare");
 const randColour = require("../../data/randomColor");
 const talkedRecently = new Set();
 
-exports.run = async (client, message, args) => {
+exports.run = async (client, message) => {
   if (
     talkedRecently.has("waifu-called") &&
     message.author.id !== "157673412561469440" &&
@@ -17,7 +17,7 @@ exports.run = async (client, message, args) => {
     message.channel.startTyping();
 
     const nightmare = Nightmare({
-      show: false,
+      show: true,
       width: 1080,
       height: 1080
     });
@@ -29,40 +29,92 @@ exports.run = async (client, message, args) => {
       .goto("https://www.thiswaifudoesnotexist.net/")
       .wait("img")
       .evaluate(getBounds, "img")
-      .end()
-      .then(async el => {
-        console.log(el);
-        //   await nightmare.end();
-        if (el.indexOf("https:") === -1) {
-          el = `https:${el}`;
+      .then(async rects => {
+        console.log(rects);
+
+        function getScreenshot(rect) {
+          nightmare
+            .screenshot({
+              x: rect.left,
+              y: rect.top,
+              width: rect.width,
+              height: rect.height
+            })
+            .end()
+            .then(buffer => {
+              const embed = new Discord.RichEmbed()
+                .setDescription(
+                  `**${message.author.username}** here is your deep learning generated waifu <:softheart:575053165804912652>`
+                )
+                .attachFiles([{ name: "waifu.png", attachment: buffer }])
+                .setImage("attachment://waifu.png")
+                .setColor(randColour())
+                .setFooter(
+                  `${message.guild.name}`,
+                  "https://cdn.discordapp.com/avatars/601825955572350976/67cca6c8e018ae7f447e6f0e41cbfd3c.png?size=2048"
+                )
+                .setTimestamp();
+              message.channel
+                .send(embed)
+                .then(() => message.channel.stopTyping(true));
+              talkedRecently.add("waifu-called");
+              setTimeout(() => {
+                talkedRecently.delete("waifu-called");
+              }, 5000);
+            })
+            .catch(function(err) {
+              console.log(err);
+              message.channel.stopTyping(true);
+            });
         }
-        const embed = new Discord.RichEmbed()
-          .setDescription(
-            `**${message.author.username}** here is your deep learning generated waifu <:softheart:575053165804912652>`
-          )
-          .setImage(el)
-          .setColor(randColour())
-          .setFooter(
-            `${message.guild.name}`,
-            "https://cdn.discordapp.com/avatars/601825955572350976/67cca6c8e018ae7f447e6f0e41cbfd3c.png?size=2048"
-          )
-          .setTimestamp();
-        message.channel
-          .send(embed)
-          .then(() => message.channel.stopTyping(true));
-        talkedRecently.add("waifu-called");
-        setTimeout(() => {
-          talkedRecently.delete("waifu-called");
-        }, 5000);
+
+        getScreenshot(rects);
       })
+
       .catch(function(err) {
         console.log(err);
         message.channel.stopTyping(true);
       });
 
     function getBounds(selector) {
-      let el = document.querySelector(selector).getAttribute("src");
-      return el;
+      // let el = document.querySelector(selector).getAttribute("src");
+      // return el;
+      let obj = {
+        top: document.querySelector(selector).offsetTop,
+        left: document.querySelector(selector).offsetLeft,
+        width: document.querySelector(selector).offsetWidth,
+        height: document.querySelector(selector).offsetHeight
+      };
+      return obj;
     }
   }
 };
+
+// .then(async el => {
+// console.log(el);
+// //   await nightmare.end();
+// if (el.indexOf("https:") === -1) {
+//   el = `https:${el}`;
+// }
+
+// const { body: buffer } = await snekfetch.get(el);
+
+// const embed = new Discord.RichEmbed()
+//   .setDescription(
+//     `**${message.author.username}** here is your deep learning generated waifu <:softheart:575053165804912652>`
+//   )
+//   // .setImage(el)
+//   .attachFiles([{ name: "waifu.png", attachment: buffer }])
+//   .setImage("attachment://waifu.png")
+//   .setColor(randColour())
+//   .setFooter(
+//     `${message.guild.name}`,
+//     "https://cdn.discordapp.com/avatars/601825955572350976/67cca6c8e018ae7f447e6f0e41cbfd3c.png?size=2048"
+//   )
+//   .setTimestamp();
+// message.channel.send(embed).then(() => message.channel.stopTyping(true));
+// talkedRecently.add("waifu-called");
+// setTimeout(() => {
+//   talkedRecently.delete("waifu-called");
+// }, 5000);
+// })
