41faacdd1721dfdf9e06c3602f59d5405a469ecb
doyle928 <doyle928@yahoo.com>
2020-02-25 17:07:19 -0500

    added mention limit, emote limit, everyone limit

--- server/events/messageUpdate.js
+++ server/events/messageUpdate.js
@@ -3,23 +3,25 @@ const serverMain = require("../data/serverMain");
 
 module.exports = async (client, messageOld, messageNew) => {
   if (messageOld.channel.guild && messageOld.embeds.length === 0) {
-    let server = serverMain.get(messageOld.guild.id);
+    if (messageOld.content !== messageNew.content) {
+      let server = serverMain.get(messageOld.guild.id);
 
-    let c = await messageOld.guild.channels.get(server.message_log);
+      let c = await messageOld.guild.channels.get(server.message_log);
 
-    if (c) {
-      let messageEmbed = new Discord.RichEmbed()
-        .setColor("#ffff00")
-        .setAuthor("Message edited")
-        .setDescription(
-          `⚠ **${messageOld.author.username}**#${messageOld.author.discriminator} (ID:${messageOld.author.id}) edited a message in ${messageOld.channel}\n**[► Message Original](https://discordapp.com/channels/${messageNew.channel.guild.id}/${messageOld.channel.id}/${messageNew.id})**\n\n**From : ** ${messageOld.content}\n**To :** ${messageNew.content}`
-        )
-        .setFooter(
-          `${messageNew.channel.guild.name}`,
-          "https://cdn.discordapp.com/avatars/601825955572350976/67cca6c8e018ae7f447e6f0e41cbfd3c.png?size=2048"
-        )
-        .setTimestamp();
-      c.send(messageEmbed);
+      if (c) {
+        let messageEmbed = new Discord.RichEmbed()
+          .setColor("#ffff00")
+          .setAuthor("Message edited")
+          .setDescription(
+            `⚠ **${messageOld.author.username}**#${messageOld.author.discriminator} (ID:${messageOld.author.id}) edited a message in ${messageOld.channel}\n**[► Message Original](https://discordapp.com/channels/${messageNew.channel.guild.id}/${messageOld.channel.id}/${messageNew.id})**\n\n**From : ** ${messageOld.content}\n**To :** ${messageNew.content}`
+          )
+          .setFooter(
+            `${messageNew.channel.guild.name}`,
+            "https://cdn.discordapp.com/avatars/601825955572350976/67cca6c8e018ae7f447e6f0e41cbfd3c.png?size=2048"
+          )
+          .setTimestamp();
+        c.send(messageEmbed);
+      }
     }
   }
 };
